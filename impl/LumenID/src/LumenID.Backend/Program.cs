c5a39349f9b1e2dc1e0c6324fb565548233cdeae
dnvs-karu3 <180717873+dnvs-karu3@users.noreply.github.com>
2025-09-08 13:03:45 +0900

    [🔨task-6] Implemented login logic.

--- impl/LumenID/src/LumenID.Backend/Program.cs
+++ impl/LumenID/src/LumenID.Backend/Program.cs
@@ -5,9 +5,12 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<TokenGenerator>();
 builder.Services.AddSingleton(sp =>
 {
-    var connectionString = Environment.GetEnvironmentVariable("ACCOUNTS_DB_CONNECTION_STRING") ?? throw new InvalidOperationException("ACCOUNTS_DB_CONNECTION_STRING environment variable is not set.");
+    var connectionString = Environment.GetEnvironmentVariable("ACCOUNTS_DB_CONNECTION_STRING") ??
+                           throw new InvalidOperationException(
+                               "ACCOUNTS_DB_CONNECTION_STRING environment variable is not set.");
 
     var options = new DbContextOptionsBuilder<AccountsDbContext>()
         .UseMySQL(connectionString)
@@ -22,6 +25,9 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<RegisterServiceImpl>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGrpcService<AuthenticateServiceImpl>();
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
