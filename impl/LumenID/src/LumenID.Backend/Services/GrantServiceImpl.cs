b3e831797e5b5cc68f60266395aca7b12b1eb93c
dnvs-karu3 <karu3_dev@noreply.dnvs.git>
2025-09-12 16:19:05 +0900

    [🔨task/7] Simplified authenticate and grant logic.

--- impl/LumenID/src/LumenID.Backend/Services/GrantServiceImpl.cs
+++ impl/LumenID/src/LumenID.Backend/Services/GrantServiceImpl.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using LumenID.Backend.Contexts.Accounts;
 using LumenID.Backend.Contexts.Clients;
@@ -16,27 +17,40 @@ public class GrantServiceImpl(
     [FromServices] AccountsDbContext accounts,
     [FromServices] ILogger<GrantServiceImpl> logger
 ) : GrantService.GrantServiceBase {
-    
+    public override async Task<Empty> Ping(Empty request, ServerCallContext context)
+    {
+        logger.LogInformation("{user}", context.AuthContext.IsPeerAuthenticated);
+        return await Task.FromResult(new Empty());
+    }
+
     [Authorize(Policy = "users")]
     public override async Task<GrantResponse> GrantApp(GrantRequest request, ServerCallContext context)
     {
+        logger.LogInformation("{user} grant permission with client {client}", context.AuthContext.IsPeerAuthenticated, request.ClientId);
         // 1. Get OAuth client data from database
         var clientMeta = await oauth.GetMetadataByPrimaryKeyAsync(request.ClientId);
-        if (clientMeta is null) throw new RpcException(new Status(StatusCode.NotFound, "Client does not exist"));
+        if (clientMeta is null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, "Client does not exist"));
+        }
         
         var clientSecret = await oauth.GetSecretByPrimaryKeyAsync(clientMeta.SecretId);
-        if (clientSecret is null) throw new RpcException(new Status(StatusCode.NotFound, $"{request.ClientId} is maybe broken. Please contact your administrator"));
+        if (clientSecret is null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, $"{request.ClientId} is maybe broken. Please contact your administrator"));
+        }
         
-        // 2. Check public key is right
-        var publicKeyBytes = Encoding.UTF8.GetBytes(request.ClientPublicKey);
-        var secretKeyBytes = Encoding.UTF8.GetBytes(clientSecret.SecretKey);
-        var hmac = HMACSHA256.HashData(secretKeyBytes, publicKeyBytes);
-        var hmacString = hmac.ToString();
-        if (hmacString is null || clientSecret.PublicValue != hmacString) throw new RpcException(new Status(StatusCode.InvalidArgument, "Public key does not match"));
+        // 2. Check redirect url is same
+        if (request.RedirectUrl != clientSecret.RedirectUrl)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Redirect URL is invalid"));
+        }
         
         // 3. Add granted apps to user account
         var userId = context.GetHttpContext().User.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Sub)?.Value;
         if (userId is null) throw new RpcException(new Status(StatusCode.NotFound, "User not found in claims"));
+        
+        logger.LogInformation("{user}", userId);
         
         var userMeta = await accounts.GetMetadataAsync(userId);
         if (userMeta is null) throw new RpcException(new Status(StatusCode.NotFound, "Account not found"));
@@ -47,9 +61,12 @@ public class GrantServiceImpl(
         try
         {
             var grantedApps = userConfig.GetGrantedApps();
-            grantedApps.AppIds.Add(request.ClientId);
+            if (grantedApps.AppIds.Contains(request.ClientId) is false)
+            {
+                grantedApps.AppIds.Add(request.ClientId);
+            }
             userConfig.SetGrantedApps(grantedApps);
-            await accounts.SaveConfigsAsync(userConfig);
+            await accounts.UpdateConfigsAsync(userConfig);
         }
         catch (Exception e)
         {
@@ -60,18 +77,19 @@ public class GrantServiceImpl(
         // 4. Generate code
         var uuid = Guid.NewGuid().ToString("N");
         var uuidBytes = Encoding.UTF8.GetBytes(uuid);
+        var secretKeyBytes = Encoding.UTF8.GetBytes(clientSecret.SecretKey);
         var uuidHmac = HMACSHA256.HashData(secretKeyBytes, uuidBytes);
-        var uuidString = uuidHmac.ToString();
+        var code = Convert.ToHexString(uuidHmac).ToLower();
         
-        if (uuidString is null or "") throw new RpcException(new Status(StatusCode.InvalidArgument, "Server can not generate code."));
+        if (code is null or "") throw new RpcException(new Status(StatusCode.InvalidArgument, "Server can not generate code."));
         
-        var newCode = oauth.CreateNewCode(uuidString, userMeta.Id, request.ClientId);
+        var newCode = oauth.CreateNewCode(uuid, code, userMeta.Id, request.ClientId);
         await oauth.SaveCodeAsync(newCode);
         
         // 5. Return response
         return new GrantResponse()
         {
-            Code = uuidString,
+            Code = code,
         };
     }
 }
