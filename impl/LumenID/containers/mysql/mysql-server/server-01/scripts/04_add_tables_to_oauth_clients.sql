b04236924633367e5fb2314a01c1114529eda4ca
dnvs-karu3 <karu3_dev@noreply.dnvs.git>
2025-09-12 16:12:28 +0900

    [🔨task/7] Remove no longer need field.

--- impl/LumenID/containers/mysql/mysql-server/server-01/scripts/04_add_tables_to_oauth_clients.sql
+++ impl/LumenID/containers/mysql/mysql-server/server-01/scripts/04_add_tables_to_oauth_clients.sql
@@ -4,8 +4,6 @@ use `oauth_clients`;
 -- Create tables
 create table if not exists `secrets` (
     `id`           varchar(36)     not null,
-    `public_key`   varchar(100)     not null,
-    `public_value` varchar(100)    not null,
     `secret_key`   varchar(64)     not null,
     `redirect_url` text            not null,
 
@@ -45,8 +43,6 @@ create table if not exists `codes` (
 -- Load data
 insert into `secrets` values (
     'b33442b7-1f5a-4970-b623-93c2588f6001',
-    'MmVlYTJmZmMtMzRmMi00NTY0LWEzNWUtZDAxYzVmMTNlNTg5',
-    '445c3c148c5c2c34a9e179c890b52ea5d92eee774ee1d6d886c68fe84ed15b77',
     'd1e57541ff5d011f6e5c37920993174e53b586aa929ca52785ae87f21c859283',
     'http://localhost:5099/oauth/callback'
 );
