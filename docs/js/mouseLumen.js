3640d6be75db65c81d7d6e2275a8c7a6a6a7948b
dnvs-karu3 <180717873+dnvs-karu3@users.noreply.github.com>
2025-09-13 09:28:01 +0900

    Add header and catch fraze for LumenID to index page.

---
+++ docs/js/mouseLumen.js
@@ -0,0 +1,37 @@
+// Get element
+const lumenMouseEl = document.getElementById("util__lumen_mouse");
+
+// Mouse positions
+let targetX = window.innerWidth / 2;
+let targetY = window.innerHeight / 2;
+let mouseX = targetX;
+let mouseY = targetY;
+
+// Smoothness
+const ease = 0.18;
+
+// Events
+window.addEventListener("mousemove", (e) => {
+    targetX = e.clientX;
+    targetY = e.clientY;
+
+    lumenMouseEl.classList.remove("hidden");
+}, {passive: true});
+window.addEventListener("mouseleave", () => lumenMouseEl.classList.add("hidden"));
+window.addEventListener("blur", () => lumenMouseEl.classList.add("hidden"));
+window.addEventListener("mouseenter", () => lumenMouseEl.classList.remove("hidden"));
+window.addEventListener("focus", () => lumenMouseEl.classList.remove("hidden"));
+
+// Mouse glow function
+function glowAnimation() {
+    // lerp
+    mouseX += (targetX - mouseX) * ease;
+    mouseY += (targetY - mouseY) * ease;
+
+    // Transform element
+    lumenMouseEl.style.transform = `translate(${mouseX - (lumenMouseEl.offsetWidth/2)}px, ${mouseY - (lumenMouseEl.offsetHeight/2)}px)`;
+
+    requestAnimationFrame(glowAnimation);
+}
+
+requestAnimationFrame(glowAnimation);
